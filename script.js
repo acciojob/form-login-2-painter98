8aa1fe8f3720a57e11ddf1edd4e2a72933837e52
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-10 17:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function result(){
+	let fname = document.getElementById('fname').value;
+	let lname = document.getElementById('lname').value;
+	let phone = document.getElementById('phone').value;
+	let email = document.getElementById('email').value;
+	let str = `First Name: ${fname} \n Last Name: ${lname} \n Phone Number: ${phone} \n Email ID: ${email}`
+	alert(str);
+}
