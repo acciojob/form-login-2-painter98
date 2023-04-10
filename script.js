ec8b6268879b2b8cf20977a5148094a8e8e0b687
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-10 17:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function result(){
 	let fname = document.getElementById('fname').value;
 	let lname = document.getElementById('lname').value;
 	let phone = document.getElementById('phone').value;
-	let email = document.getElementById('email').value;
+	let email = document.getElementById('email').value;//get the values
+
+	//make the things to be shown in alert to string 
 	let str = `First Name: ${fname} \n Last Name: ${lname} \n Phone Number: ${phone} \n Email ID: ${email}`
 	alert(str);
 }
